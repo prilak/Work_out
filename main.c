ba310741a314c709d1118d98270e20321726a10c
Michael <mdlawrence@utexas.edu>
2017-01-25 19:12:01 -0600

    First commit for Exercise app in C
    muscle sort is created

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int arms = 0;
+int legs = 1;
+
+int mus_group_sort(int typ) {
+    if(typ == arms) {
+        printf("arms/n");
+        return arms;
+    }
+    if(typ == legs){
+        printf("legs/n");
+        return legs;
+    }
+    
+}
+
+
+int main(void) {
+    srand(time(NULL));
+    int randy = rand() % 2;
+    printf("%d", randy);
+    
+    int mus_group = mus_group_sort(randy);
+    
+    return 0;
+}
